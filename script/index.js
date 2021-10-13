774f7d8e13d1900e53e226048a20f8ee8eb7ae05
Ryan Donald <ryanthomasdonald@gmail.com>
2021-10-13 12:01:08 -0400

    form working

--- script/index.js
+++ script/index.js
@@ -3,8 +3,8 @@ let month = document.querySelector("#month");
 let day = document.querySelector("#day");
 
 submitButton.addEventListener("click", (e) => {
-    e.preventDefault()
-    console.log(month.value + "/" + day.value)
-    let date = month.value + "/" + day.value
-    return date
-})
+    e.preventDefault();
+    console.log(month.value + "/" + day.value);
+    let date = month.value + "/" + day.value;
+    return date;
+});
