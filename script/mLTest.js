e2b067ee94c733abb266465b63e1ce2683f55461
Victoria Walker <vmsmith2014@gmail.com>
2021-10-13 09:30:19 -0400

    latitude, longitude selectors

--- script/mLTest.js
+++ script/mLTest.js
@@ -13,9 +13,16 @@ fetch(`https://byabbe.se/on-this-day/${date}/events.json`).then(result => {
     // let firstWikiLinkObj = wikiLinksList[0];
     // let firstWikiLink = firstWikiLinkObj.wikipedia;
     // // console.log(firstWikiLink);
-    let firstWikiLink = "https://en.m.wikipedia.org/wiki/Battle_of_Gettysburg"
+
+    // test items:
+    // let firstWikiLink = "https://en.m.wikipedia.org/wiki/Battle_of_Gettysburg"
+    // let firstWikiLink = "https://en.wikipedia.org/wiki/South_Carolina"
+    // let firstWikiLink = "https://en.wikipedia.org/wiki/Tandil"
+    let firstWikiLink = "https://en.wikipedia.org/wiki/Tokelau"
+    
 
     fetch(firstWikiLink).then(result => {
+        // console.log(result);
         return result.text()
     })
     .then(data => {
@@ -24,15 +31,27 @@ fetch(`https://byabbe.se/on-this-day/${date}/events.json`).then(result => {
         let doc = parser.parseFromString(data, 'text/html');
         // console.log(doc)
 
-        if (doc.querySelector(".geo-dec")) {
-            let parent = doc.querySelector(".geo-dec");
-            let content = parent.textContent;
-            let contentArray = content.split("°");
-            let long = contentArray[0];
-            let contentArray2 = contentArray[1].split(" ")
-            let lat = contentArray2[1]
-            return lat, long
+        // if (doc.querySelector(".geo-dec")) {
+        //     let parent = doc.querySelector(".geo-dec");
+        //     console.log(parent);
+        //     let content = parent.textContent;
+        //     let contentArray = content.split("°");
+        //     let long = contentArray[0];
+        //     let contentArray2 = contentArray[1].split(" ");
+        //     let lat = contentArray2[1];
+        //     console.log(long, lat);
+        // }
+        if (doc.querySelector(".latitude") && doc.querySelector(".longitude")) {
+            console.log("hi");
+            let longSpan = doc.querySelector(".longitude").textContent
+            console.log(longSpan);
+            let latSpan = doc.querySelector(".latitude").textContent;
+            console.log(latSpan);
+            // need to split on degrees and spaces again... ask Andrew about this one
         }
+        // else if (doc.querySelector(".geo-dms")) {
+            
+        // }
         else {
             console.log("error")
         }
