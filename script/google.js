b0eace209fdb18040835348e186c0595f85fab76
AndrewHatch <03110h@gmail.com>
2021-10-14 15:39:51 -0400

    nightmode edits

--- script/google.js
+++ script/google.js
@@ -23,102 +23,96 @@ let options = {
     center: { lat: 30, lng:  15 },
     mapTypeControl: false,
     styles: [
-        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+        { elementType: "geometry", stylers: [{ color: "#585858" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#000000" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#01FEFF" }] },
         {
-          featureType: "administrative.locality",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }],
-        },
-        {
-          featureType: "poi",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }],
-        },
-        {
-          featureType: "poi.park",
-          elementType: "geometry",
-          stylers: [{ color: "#263c3f" }],
-        },
-        {
-          featureType: "poi.park",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#6b9a76" }],
-        },
-        {
-          featureType: "road",
-          elementType: "geometry",
-          stylers: [{ color: "#38414e" }],
-        },
-        {
-          featureType: "road",
-          elementType: "geometry.stroke",
-          stylers: [{ color: "#212a37" }],
-        },
-        {
-          featureType: "road",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#9ca5b3" }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "geometry",
-          stylers: [{ color: "#746855" }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "geometry.stroke",
-          stylers: [{ color: "#1f2835" }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#f3d19c" }],
-        },
-        {
-          featureType: "transit",
-          elementType: "geometry",
-          stylers: [{ color: "#2f3948" }],
-        },
-        {
-          featureType: "transit.station",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }],
-        },
-        {
-          featureType: "water",
-          elementType: "geometry",
-          stylers: [{ color: "#17263c" }],
-        },
-        {
-          featureType: "water",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#515c6d" }],
-        },
-        {
-          featureType: "water",
-          elementType: "labels.text.stroke",
-          stylers: [{ color: "#17263c" }],
-        },
-      ],
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#CB00F8" }],
+    },
+    {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#ffffff" }],
+    },
+    {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }],
+    },
+    {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }],
+    },
+    {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#000000" }],
+    },
+    {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }],
+    },
+    {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }],
+    },
+    {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#000000" }],
+    },
+    {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }],
+    },
+    {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }],
+    },
+    {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }],
+    },
+    {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#ffffff" }],
+    },
+    {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#000000" }],
+    },
+    {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#000000" }],
+    },
+    {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#000000" }],
+    },
+    ],
     };
     
 function initMap() {
-
-
-
 googleMap = new google.maps.Map(document.getElementById("googleMap"),options);
 geocoder = new google.maps.Geocoder();
 infowindow = new google.maps.InfoWindow();
 marker = new google.maps.Marker({googleMap,});
 marker2 = new google.maps.Marker({googleMap,});
 marker3 = new google.maps.Marker({googleMap,});
-
-let array = chosenEventsList
-
 googleMap.addListener("click", (e) => {
-sortObj(array)
+sortObj(chosenEventsList)
 });
 }
 
@@ -126,16 +120,18 @@ sortObj(array)
 function sortObj(objArray){
     for(let i = 0; i < objArray.length; i++){
         let markerArr = [marker,marker2,marker3]
-    if (objArray[i].locationType == 'country'){
-        geocode({ address: objArray[i].country },markerArr[i]);
-    }
-    else if(objArray[i].locationType == 'coord'){
-        let lat = objArray[i].latlng.lat
-        let long = objArray[i].latlng.lng
-        geocodeLatLng(geocoder, googleMap, infowindow, lat, long, markerArr[i])
-    }
+            if (objArray[i].locationType == 'country'){
+                geocode({ address: objArray[i].country },markerArr[i]);
+            }
+            else if(objArray[i].locationType == 'coord'){
+                let lat = objArray[i].latlng.lat
+                let long = objArray[i].latlng.lng
+                geocodeLatLng(geocoder, googleMap, infowindow, lat, long, markerArr[i])
+            }
 }}
 // function to process grid coordinates
+
+
 function geocodeLatLng(geocoder, googleMap, infowindow,lat,long, markVar) {
 latlong = {
     lat: parseFloat(lat),
@@ -155,6 +151,8 @@ geocoder.geocode({ location: latlong })
     .catch((e) => window.alert("Geocoder failed due to: " + e));
 }
 //function to process country 
+
+
 function geocode(request,markVar,infowindow) {
 geocoder.geocode(request)
 .then((result) => {
